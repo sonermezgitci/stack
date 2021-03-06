9c727b43197006ba77149e94aa9147db5fa52aa6
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-05 23:36:23 -0500

    DDLexercise-shift

---
+++ DddlShiftExercise.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.length = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    shift() {
+        if (!this.head) return undefined;
+        var oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+}
