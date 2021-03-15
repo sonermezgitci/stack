ff127b00f4cc873bd69f910d6c868fc8d30f392f
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-15 00:49:41 -0400

    DLLReverseExercise

---
+++ DDLReverseExercise.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        var node = new Node(val);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = this.head;
+            this.length++;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+            this.length++;
+        }
+        return this;
+    }
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+}
