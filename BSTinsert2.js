92203db0c570bfe32054838fec2c0109c1e90384
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-24 22:13:51 -0400

    BTSinsert3

---
+++ BSTinsert2.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+
+    }
+
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root
+            while (true) {
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this
+                    } else {
+                        current = current.left;
+                    }
+
+                }
+            }
+        }
+
+    }
+
+}
+
+
+
+var tree = new BinarySearchTree();
+tree.insert(14)
+tree.insert(8)
+tree.insert(15)
+tree.insert(5)
+tree.insert(22)
+tree.insert(16)
+tree.insert(9)
+console.log(tree)
