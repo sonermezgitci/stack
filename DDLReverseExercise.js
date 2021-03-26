f55370aaf3b2adcba9c1fb3ee09d406d45d40736
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-26 19:17:11 -0400

    DDLReverseTest2

--- DDLReverseExercise.js
+++ DDLReverseExercise.js
@@ -27,19 +27,5 @@ class DoublyLinkedList {
         }
         return this;
     }
-    reverse() {
-        var node = this.head;
-        this.head = this.tail;
-        this.tail = node;
-        var next;
-        var prev = null;
 
-        for (let i = 0; i < this.length; i++) {
-            next = node.next;
-            node.next = prev;
-            prev = node;
-            node = next;
-        }
-        return this;
-    }
 }
