149b1156d4ee179002cba0b382e8bea80dd78156
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-25 20:59:51 -0400

    OneBitProblem2

--- LeedOneBit.js
+++ LeedOneBit.js
@@ -2,15 +2,22 @@
  * @param {number[]} bits
  * @return {boolean}
  */
+
 var isOneBitCharacter = function (bits) {
-    // var oneBit = 0;
-    // var twoBit =10;
-    for (let i = 0; i < bits.length; i++) {
-        if (bits[i] === 0) {
-            return true
-        } else if (bits[i] == 10) {
-            return false
-        }
+    var i = 0;
+    while (i < bits.length - 1) {
+        i += bits[i] + 1;
+
     }
+    return i == bits.length - 1;
 
 };
+
+
+// Solution Explanation 
+
+
+// When reading from the i-th position, if bits[i] == 0,
+//  the next character must have 1 bit; else if bits[i] == 1, 
+// the next character must have 2 bits. We increment our read-pointer i to the start of the next character appropriately.
+//  At the end, if our pointer is at bits.length - 1, then the last character must have a size of 1 bit.
