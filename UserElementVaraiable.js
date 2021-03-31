331fa0713abf6053b36ca32a80976b01236aabda
Soner Mezgitci <mezgitci9@gmail.com>
2021-03-31 18:01:54 -0400

    Element Variable

---
+++ UserElementVaraiable.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+
+    render() {
+        let message
+        if (this.state.isLoggedIn) {
+            message = <div> Welcome Soner</div>
+        } else {
+            message = <div> Welcome Guest </div>
+
+        }
+        return <div> {message}</div>
+
+    }
+}
+
+export default UserGreeting
