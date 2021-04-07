e7199bda66bb62b9c509418908d96bfff4acfd6c
Soner Mezgitci <mezgitci9@gmail.com>
2021-04-06 21:59:33 -0400

    Flatten a  Dictionary

---
+++ flattenDictionary.js
@@ -0,0 +1,49 @@
+function flattenDictionary(dict) {
+    let newDict = {}
+    /*
+    for (const [key, value] of dict.entries()) {
+    console.log(key, value);
+    typeof value; 
+  }
+    
+    */
+
+    for (const [key, value] of dict.entries()) {
+        if (typeof value === "string") {
+            newDict.push(key, value);
+        } else {
+            // recursion
+
+            newSubDict = callRecursiveFuncion(value)
+            for (newSubDict) {
+                newKey = Key + subKey
+                newDict.concat(newKey, subvalue)
+            }
+
+        }
+    }
+} function flattenDictionary(dict) {
+    let newDict = {}
+    /*
+    for (const [key, value] of dict.entries()) {
+    console.log(key, value);
+    typeof value; 
+  }
+    
+    */
+
+    for (const [key, value] of dict.entries()) {
+        if (typeof value === "string") {
+            newDict.push(key, value);
+        } else {
+            // recursion
+
+            newSubDict = callRecursiveFuncion(value)
+            for (newSubDict) {
+                newKey = Key + subKey
+                newDict.concat(newKey, subvalue)
+            }
+
+        }
+    }
+}
