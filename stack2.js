49a44b4ac689fbb3c7533c6566594cbcd846ba7a
Soner Mezgitci <mezgitci9@gmail.com>
2021-04-11 20:42:00 -0400

    Stack2

---
+++ stack2.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        var newNode = new Node(val)
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp
+
+        }
+        return ++this.size
+
+    }
+
+    pop() {
+        if (!this.first) return null;
+        var current = this.first;
+        if (this.first === 1) {
+            //              this.first = null;
+            this.last = null;
+
+        } else {
+            this.first = current.next;
+            this.size--;
+
+        }
+        return current.value
+
+    }
+
+}
