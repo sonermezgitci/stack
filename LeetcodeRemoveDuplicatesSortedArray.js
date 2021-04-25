beacf4fba94cd27ca709f083215989a2b15b48e0
Soner Mezgitci <mezgitci9@gmail.com>
2021-04-25 00:58:20 -0400

    Remove duplicate from sorted array

--- LeetcodeRemoveDuplicatesSortedArray.js
+++ LeetcodeRemoveDuplicatesSortedArray.js
@@ -1,13 +1,43 @@
+
 var removeDuplicates = function (nums) {
-    const len = nums.length;
-    for (i = 0; i < len - 1; i++) {
-        if (i > nums.length - 1) {
-            break;
-        }
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i, 1);
-            i--;
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let pointer1 = 0;
+
+    for (let pointer2 = 1; pointer2 < nums.length; pointer2++) {
+        if (nums[pointer1] !== nums[pointer2]) {
+            pointer1++;
+            nums[pointer1] == nums[pointer2];
         }
     }
-    return nums.length;
-};
+    return pointer1 + 1
+
+}
+
+//   Not clear which function taking unique number frot of the array;
+//   What is the pupose of the return  inceremented pointer1 + 1 on the end 
+
+
+
+
+
+
+
+
+
+
+// var removeDuplicates = function (nums) {
+//     const len = nums.length;
+//     for (i = 0; i < len - 1; i++) {
+//         if (i > nums.length - 1) {
+//             break;
+//         }
+//         if (nums[i] === nums[i + 1]) {
+//             nums.splice(i, 1);
+//             i--;
+//         }
+//     }
+//     return nums.length;
+// };
