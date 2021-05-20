35786dfecf43a7e919aeb1234f7f82a0d46ead50
Soner Mezgitci <mezgitci9@gmail.com>
2021-05-20 00:16:07 -0400

    leetOddMedium

---
+++ leetOddMedium.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function oddEvenList(head) {
+    if (!head) return head;
+
+    var odd = head;
+    var even = head.next;
+    while (odd.next && odd.next.next) {
+        var tmp = odd.next;
+        odd.next = odd.next.next;
+        odd = odd.next;
+        tmp.next = odd.next;
+    }
+    odd.next = even;
+    return head;
+}
