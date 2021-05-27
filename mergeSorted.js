21cc7461117c8269e2ef40562da81d6d14290d0d
Soner Mezgitci <mezgitci9@gmail.com>
2021-05-26 21:07:25 -0400

    Merge Sorted

---
+++ mergeSorted.js
@@ -0,0 +1,12 @@
+var merge = function (nums1, m, nums2, n) {
+    while (n) {
+        if (nums1[m - 1] > nums2[n - 1]) {
+            nums1[m + n - 1] = nums1[m - 1];
+            m--;
+        } else {
+            nums1[m + n - 1] = nums2[n - 1];
+            n--;
+        }
+    }
+    return nums1;
+};
