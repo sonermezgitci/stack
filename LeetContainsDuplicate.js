5eeb9c06b3af51aa63a80e029f9d96dd3692f79e
Soner Mezgitci <mezgitci9@gmail.com>
2021-05-31 00:30:01 -0400

    LeetContains Duplicate

---
+++ LeetContainsDuplicate.js
@@ -0,0 +1,16 @@
+var containsDuplicate = function (nums) {
+    let numbers = new Set();
+    for (let num of nums) {
+        if (!numbers.has(num)) {
+            numbers.add(num)
+        } else {
+            return true
+
+        }
+
+    }
+
+    return false
+
+
+}
