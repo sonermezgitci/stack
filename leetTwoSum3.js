e856dab4ca92560929a5e1c135132bd1fc1e332a
Soner Mezgitci <mezgitci9@gmail.com>
2021-06-01 00:17:36 -0400

    TwoSum3

---
+++ leetTwoSum3.js
@@ -0,0 +1,14 @@
+var twoSum = function (nums, target) {
+    let numberIndex = new Map();
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        let complement = target - num;
+        if (numberIndex.has(complement)) {
+            result[0] = numberIndex.get(complement)
+            result[1] = i;
+            return result;
+        }
+        numberIndex.set(num, i)
+    }
+}
