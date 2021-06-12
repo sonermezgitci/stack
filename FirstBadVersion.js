421e6cd8368f2b8fbb1276b020975ad9775a4768
Soner Mezgitci <mezgitci9@gmail.com>
2021-06-12 00:22:14 -0400

    FirstBadVersion.js

---
+++ FirstBadVersion.js
@@ -0,0 +1,18 @@
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1, right = n
+        while (left < right) {
+            const mid = Math.floor((right + left) / 2)
+            if (isBadVersion(mid)) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return right
+    };
+};
