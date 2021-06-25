8786971f9a59eaba59fa6b4a93f5215e8f2a9a08
Soner Mezgitci <mezgitci9@gmail.com>
2021-06-25 03:32:31 -0400

    MaxArray2

---
+++ MaxArray.js
@@ -0,0 +1,11 @@
+var maxSubArray = function (nums) {
+    if (nums.length == 0) return 0;
+    let result = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        result = Math.max(sum, result);
+        sum = sum < 0 ? 0 : sum;
+    }
+    return result;
+};
