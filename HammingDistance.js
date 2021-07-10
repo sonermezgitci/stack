cb186e19d5e2cd2c2ab484e91e4c4dbd5df90e61
Soner Mezgitci <mezgitci9@gmail.com>
2021-07-10 00:57:14 -0400

    HammingDistance

---
+++ HammingDistance.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+
+    var xBase = x.toString(2).split("");
+    var yBase = y.toString(2).split("");
+    var distance = 0;
+    while (xBase.length < yBase.length) {
+        xBase.unshift("0");
+    }
+    while (xBase.length > yBase.length) {
+        yBase.unshift("0");
+    }
+
+    xBase.forEach((x, i) => {
+        if (x !== yBase[i]) {
+            distance++
+        }
+    })
+    return distance;
+};
